60441ba956dcb831f0e84aed40415235acc84c07
saeed <mostafazarei13751375@gmail.com>
2023-08-13 00:52:53 +0330

    note

---
+++ app.js
@@ -0,0 +1,54 @@
+// let textVorodi=document.querySelector('.text-vorodi')
+// let noteClick=document.querySelector('.note-click')
+// let form=document.querySelector('#note-form')
+// let noteMatne=document.querySelector('.note-matne')
+// let noteUl=document.querySelector('.note-ul')
+// let newLi=document.createElement('li')
+// console.log(noteClick)
+// console.log(textVorodi)
+
+// textVorodi.addEventListener('input',()=>{
+//     // console.log(textVorodi.value)
+// })
+// noteClick.addEventListener('click',()=>{
+//     // console.log(textVorodi.value)
+// })
+// form.addEventListener('submit',(e)=>{
+//     e.preventDefault()
+//     console.log(textVorodi.value)
+    
+// })
+// form.addEventListener('submit',(e)=>{
+//     e.preventDefault()
+//     // noteMatne.innerText=`${textVorodi.value}`
+//     let newLi=document.createElement('li')
+//     newLi.innerHTML=textVorodi.value
+//     document.body.appendChild(newLi)
+//     form.reset();
+//     // noteMatne=document.createElement('a')
+//     // noteMatne.setAttribute('href','#')
+//     // noteMatne.setAttribute('innerHTML','X')
+//     // noteMatne.textContent='X'
+//     // noteMatne.appendChild(newLi)
+//     // console.log(noteMatne)
+  
+// })
+
+
+// // let span =document.createElement('span')
+// // span.innerText=textVorodi
+// // let li = document.createElement('li')
+// // li.appendChild=(span)
+// // let a=document.createElement('a')
+// // a.innerHTML='X'
+// // li.appendChild(a)
+// // console.log(li)
+
+
+// // todo search about localStorage sessionstorage
+// // creat element and append element must happend in event listener so every time you submit new element can be crated
+
+// // you need to append new element to section that you have selected not body
+
+// // search about array method like filter() and map() 
+// // try to save every all note in arry so you can loade them in start of app
